5e85e60837f7a5e95d9885c7607bffb9b484947a
Abdurrahman-Hassan <sufyanhassan123321@gmail.com>
2024-01-23 20:38:24 +0500

    fix routing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,16 @@ const Hero = ({ data }) => {
 
   return (
     <div className="max-w-md bg-[#FFDB58] rounded-md overflow-hidden shadow-md">
-      <img className="w-full h-48 object-cover" src={strMealThumb} alt={strMeal} />
-      <div className="p-4">
-        <h2 className="text-2xl font-bold mb-2 text-white">{strMeal}</h2>
-        <Link href={`/recipe/${idMeal}`}>
-        </Link>
-      </div>
+      <Link href={`/Receipe/${idMeal}`}>
+        <img
+          className="w-full h-48 object-cover"
+          src={strMealThumb}
+          alt={strMeal}
+        />
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-2 text-white">{strMeal}</h2>
+        </div>
+      </Link>
     </div>
   );
 };
